c30cf74ac140dde59a096ad94bf09af9fe4ec0f5
bodzile <155837912+bodzile@users.noreply.github.com>
2026-05-10 18:00:14 +0200

    Added api.js for all api calls

---
+++ src/Services/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = "https://dummyjson.com/products";
+
+// const random = () => {};
+
+export const getProducts = () => {
+    return axios.get(API_URL)
+        .then((response) => {
+            return response.data.products;
+        });
+};
+
+export default getProducts;
