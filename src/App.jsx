71c6ee181d2e15d48f432dc769967262fe72dc05
bodzile <155837912+bodzile@users.noreply.github.com>
2026-05-10 15:53:41 +0200

    Added pages and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Poz od svih poz</h1>
-      <h4 style={{"color": "var(--color-accent)"}}>Some element</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum reiciendis rerum totam. Assumenda blanditiis, consectetur debitis distinctio, in ipsa, laudantium maxime molestiae nesciunt numquam obcaecati pariatur recusandae saepe sed voluptatem?</p>
+
     </>
   )
 }
