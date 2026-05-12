7b80dd0a8750bf75849ae58a9c0040ad5fc79538
bodzile <155837912+bodzile@users.noreply.github.com>
2026-05-13 00:23:27 +0200

    Changed colors of Modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -50,11 +50,11 @@ const Modal = ({isOpen, onCloseConfirm, onCloseCancel, productData, resetProduct
 
     return (
         <div className="fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-black/60 backdrop-blur-sm">
-            <div className="relative m-4 w-2/5 min-w-[40%] max-w-[40%] rounded-lg bg-white p-4 shadow-sm">
-                <div className="flex shrink-0 items-center pb-4 text-xl font-medium text-slate-800">
+            <div className="relative m-4 w-2/5 min-w-[40%] max-w-[40%] rounded-lg bc-surface-dim text-text p-4 shadow-sm">
+                <div className="flex shrink-0 items-center pb-4 text-xl font-medium">
                     Add product
                 </div>
-                <div className="relative border-t border-slate-200 py-4 font-light leading-normal text-slate-600">
+                <div className="relative border-t border-slate-200 py-4 font-light leading-normal">
                     <form className="flex flex-col gap-3" action="">
                         <div className="relative flex items-center justify-between gap-4">
                             <label htmlFor="title">Title</label>
@@ -81,12 +81,12 @@ const Modal = ({isOpen, onCloseConfirm, onCloseCancel, productData, resetProduct
                 </div>
                 <div className="flex shrink-0 flex-wrap items-center justify-end pt-4">
                     <button onClick={cancelAction}
-                        className="rounded-md border border-transparent px-4 py-2 text-center text-sm text-slate-600 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        className="rounded-md border border-transparent px-4 py-2 text-center text-sm  transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button">
                         Cancel
                     </button>
                     <button onClick={confirmAction}
-                        className="ml-2 rounded-md border border-transparent bg-green-600 px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        className="ml-2 rounded-md border border-transparent bg-green-600 px-4 py-2 text-center text-sm  shadow-md transition-all hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button">
                         Confirm
                     </button>
